dc5fe6650ca1e83636c4b30030a81320b22e07a3
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-09-13 20:54:17 +0200

    enable republish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 VERSION="v$(cat src/main/resources/version.xml | sed -E 's#<version>([^<]+)</version>#\1#')"
 echo publishing $VERSION
+git tag -d $VERSION || true
+git push origin :refs/tags/$VERSION || true
 git tag $VERSION
 git push origin $VERSION
